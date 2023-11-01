afcc7c6ab885446e179e5ab83b9c37aa56f49a9b
Aldikrasniqi <aldikrasniqi5@gmail.com>
2023-11-02 00:03:34 +0100

    Login,Registered and Profile page modified

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,1 +1,34 @@
-export default function Profile() {}
+import React from 'react';
+import { getCurrentUser } from '../services/auth-service';
+// @ts-ignore
+const Profile: React.FC = () => {
+  const currentUser = getCurrentUser();
+
+  return (
+    <div className="container">
+      <h3>{currentUser.username} Profile</h3>
+
+      <header>
+        <p>
+          <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{' '}
+          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+        </p>
+        <p>
+          <strong>Id:</strong> {currentUser.id}
+        </p>
+        <p>
+          <strong>Email:</strong> {currentUser.email}
+        </p>
+        <strong>Authorities:</strong>
+        <ul>
+          {currentUser.roles &&
+            currentUser.roles.map((role: string, index: number) => (
+              <li key={index}>{role}</li>
+            ))}
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default Profile;
