22647f14e486c4ca82f83ed80c51ff57a8f92837
Aldikrasniqi <aldikrasniqi5@gmail.com>
2023-11-01 20:47:10 +0100

    Project stared

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="font-bold text-3xl">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/user-service.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { API_URL } from '../constants/api';
+import authHeader from './auth-header';
+
+export const getPublicContent = () => {
+  return axios.get(API_URL + 'all');
+
+};
+
+export const getUserBoard = () => {
+  return axios.get(API_URL + 'user', { headers: authHeader() });
+
+};
+
+export const getModeratorBoard = () => {
+  return axios.get(API_URL + 'mod', { headers: authHeader() });
+
+};
+
+export const getAdminBoard = () => {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    
+}
