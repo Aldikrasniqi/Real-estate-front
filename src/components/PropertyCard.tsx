0747373eb80be9d5164522acc6ca0a13d5586c49
Aldikrasniqi <aldikrasniqi5@gmail.com>
2023-11-14 13:36:04 +0100

    Property card implemented with testing props

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -13,87 +13,75 @@ type Props = {
 const PropertyCard = (props: { propertyData: Props[] }) => {
   return (
     <>
-      {/* <div className="flex flex-1 gap-4 m-4">
-          {props.propertyData.map((property, index) => (
-            <div key={index}>
-              {property.propImg.map((img, imgIndex) => (
-                // @ts-nocheck
-                <img
-                  key={imgIndex}
-                  src={img}
-                  alt={`Property ${index + 1} - Image ${imgIndex + 1}`}
-                />
-              ))}
-              <p>{property.propType}</p>
-              <p>{property.bedroom}</p>
-              <p>{property.bath}</p>
-              <p>{property.area}</p>
+        {props.propertyData.map((property, index) => (
+          <div key={index} className="flex flex-col lg:w-1/2 p-4 h-[530px] rounded-lg overflow-hidden">
+            {/* @ts-ignore */}
+            {property.propImg.map((img, imgIndex) => (
+              // @ts-ignore
+              <img
+                key={imgIndex}
+                src={img}
+                alt={`Property ${index + 1} - Image ${imgIndex + 1}`}
+                className="h-full rounded-t-lg object-cover object-center overflow-hidden"
+              />
+            ))}
+            <div className="bg-[#161616] p-4">
+              <div className="flex flex-row justify-between">
+                <h1 className="text-[#FFFBFB] text-2xl font-semibold">{property.propType}</h1>
+                <button
+                  style={{
+                    borderRadius: '0.5rem',
+                    background: 'linear-gradient(90deg, #FEAC6D 0%, #AE61ED 100%)',
+                    boxShadow: '0px 17px 33px 0px rgba(255, 255, 255, 0.20)',
+                    padding: '0.5rem 1rem', // Adjust padding as needed
+                    color: '#FFF', // Text color
+                    border: 'none', // Remove default button border
+                    cursor: 'pointer', // Add pointer cursor on hover
+                  }}
+                >
+                  View details
+                </button>
+              </div>
+              <div className="flex flex-row justify-center items-center rounded pt-4 w-full">
+                <span
+                  className="w-1/3 text-center border rounded-l-md p-2"
+                  style={{
+                    borderRightWidth: '1px',
+                    borderRightColor: 'rgba(255, 251, 251, 0.35)',
+                    borderColor: 'rgba(255, 251, 251, 0.35)',
+                    color: 'rgba(255, 251, 251, 0.65)',
+                  }}
+                >
+                  {property.bedroom} Bedroom
+                </span>
+                <span
+                  className="w-1/3 text-center border p-2"
+                  style={{
+                    borderColor: 'rgba(255, 251, 251, 0.35)',
+                    borderRightWidth: '0px',
+                    borderRightColor: 'rgba(255, 251, 251, 0.35)',
+                    borderLeftWidth: '0px',
+                    borderLeftColor: 'rgba(255, 251, 251, 0.35)',
+                    color: 'rgba(255, 251, 251, 0.65)',
+                  }}
+                >
+                  {property.bath} Baths
+                </span>
+                <span
+                  className="w-1/3 text-center border rounded-r-md p-2"
+                  style={{
+                    borderColor: 'rgba(255, 251, 251, 0.35)',
+                    borderLeftWidth: '1px',
+                    borderLeftColor: 'rgba(255, 251, 251, 0.35)',
+                    color: 'rgba(255, 251, 251, 0.65)',
+                  }}
+                >
+                  {property.area} sq ft
+                </span>
+              </div>
             </div>
-          ))}
-        </div> */}
-      <div className="flex flex-col w-1/2 p-4 h-[530px] rounded-lg">
-        <img
-          src="https://images.pexels.com/photos/1824392/pexels-photo-1824392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className="h-full rounded-t-lg object-cover object-center"
-        />
-        <div className="bg-[#161616] p-4">
-          <div className="flex flex-row justify-between">
-            <h1 className="text-[#FFFBFB] text-2xl font-semibold">100,000$</h1>
-            <button
-              style={{
-                borderRadius: '0.5rem',
-                background: 'linear-gradient(90deg, #FEAC6D 0%, #AE61ED 100%)',
-                boxShadow: '0px 17px 33px 0px rgba(255, 255, 255, 0.20)',
-                padding: '0.5rem 1rem', // Adjust padding as needed
-                color: '#FFF', // Text color
-                border: 'none', // Remove default button border
-                cursor: 'pointer', // Add pointer cursor on hover
-              }}
-            >
-              View details
-            </button>
           </div>
-          <div className="flex flex-row justify-center items-center rounded pt-4 w-full">
-            <span
-              className="w-1/3 text-center border rounded-l-md p-2"
-              style={{
-                borderRightWidth: '1px',
-                borderRightColor: 'rgba(255, 251, 251, 0.35)',
-                borderColor: 'rgba(255, 251, 251, 0.35)',
-                color: 'rgba(255, 251, 251, 0.65)',
-              }}
-            >
-              1 Bedroom
-            </span>
-            <span
-              className="w-1/3 text-center border p-2"
-              style={{
-                borderColor: 'rgba(255, 251, 251, 0.35)',
-                borderRightWidth: '0px',
-                borderRightColor: 'rgba(255, 251, 251, 0.35)',
-                borderLeftWidth: '0px',
-                borderLeftColor: 'rgba(255, 251, 251, 0.35)',
-                color: 'rgba(255, 251, 251, 0.65)',
-              }}
-            >
-              3 Baths
-            </span>
-            <span
-              className="w-1/3 text-center border rounded-r-md p-2"
-              style={{
-                borderColor: 'rgba(255, 251, 251, 0.35)',
-                borderLeftWidth: '1px',
-                borderLeftColor: 'rgba(255, 251, 251, 0.35)',
-                color: 'rgba(255, 251, 251, 0.65)',
-              }}
-            >
-              535 sq ft
-            </span>
-          </div>
-        </div>
-      </div>
-      
+        ))}
     </>
   );
 };
--- src/libs/data.js
+++ src/libs/data.js
@@ -2,21 +2,34 @@ export const propertyData = [
   {
     id: '1',
     propType: 'Apartment',
+    agent: 'John Doe',
     bedroom: 2,
     bath: 1,
     area: 100,
     propImg: [
-      'https://www.santarosaforward.com/img/managed/Image/111/file.jpg',
+      'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     ],
   },
   {
     id: '2',
     propType: 'Home',
+    agent: 'Jane Doe',
     bedroom: 22,
     bath: 11,
     area: 102.1,
     propImg: [
-      'https://www.santarosaforward.com/img/managed/Image/111/file.jpg',
+      'https://images.pexels.com/photos/13752348/pexels-photo-13752348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    ],
+  },
+  {
+    id: '3',
+    propType: 'Home',
+    agent: 'John Doe',
+    bedroom: 4,
+    bath: 2,
+    area: 132.1,
+    propImg: [
+      'https://images.pexels.com/photos/10610733/pexels-photo-10610733.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     ],
   },
 ];
